e08d570ff36855dfceb507aeecfb8a8b38cb585a
Ran Wei <vivianwrvision@gmail.com>
2016-08-03 18:39:15 -0400

    Create AbstractMarkovModel.java

---
+++ AbstractMarkovModel.java
@@ -0,0 +1,25 @@
+
+/**
+ * Abstract class AbstractMarkovModel 
+ * 
+ * @author Vivian
+ * @version 1.0
+ */
+
+import java.util.*;
+
+public abstract class AbstractMarkovModel implements IMarkovModel {
+    protected String myText;
+    protected Random myRandom;
+    
+    public AbstractMarkovModel() {
+        myRandom = new Random();
+    }
+    
+    public void setTraining(String s) {
+        myText = s.trim();
+    }
+ 
+    abstract public String getRandomText(int numChars);
+
+}
